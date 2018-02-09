5ee26c831c9236f7562f88e7b91ecc0fc9b38a62
fmossott <franco.mossotto@hcl.com>
2018-02-09 12:13:32 -0500

    Added docker installation

---
+++ docker/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#############################################################################
+# Licensed Materials - Property of HCL*
+# (C) Copyright HCL Technologies Ltd. 2017, 2018 All rights reserved.
+# * Trademark of HCL Technologies Limited
+#############################################################################
+
+here=`dirname $0`
+basedir=$here/..
+
+$here/inst-docker.sh
